5f0860deb3b4219c1d2d0149dd94491a4d43598d
Coldstars <hanxing950123@163.com>
2022-09-15 22:43:34 +0800

    准备事项

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,49 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
+import styled from "styled-components";
+import "./index.scss";
+const Nav = styled.div`
+  color: red;
+`;
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Router>
+        <ul>
+          <li>
+            <Link to="/">Home</Link>
+          </li>
+          <li>
+            <Link to="/About">About</Link>
+          </li>
+          <li>
+            <Link to="/Users">Users</Link>
+          </li>
+        </ul>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/About" element={<About />} />
+          <Route path="/Users" element={<Users />} />
+          <Route path="/*" element={<NoMatch />} />
+        </Routes>
+      </Router>
     </div>
   );
+}
+function Home() {
+  return <h2>Home</h2>;
+}
+
+function About() {
+  return <h2>About</h2>;
+}
+
+function Users() {
+  return <h2>Users</h2>;
+}
+function NoMatch() {
+  return <h2>NoMatch</h2>;
 }
 
 export default App;
