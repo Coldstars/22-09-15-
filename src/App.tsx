27c7d44da7efe54bb41ff7d2c54a36cd498e7689
Coldstars <hanxing950123@163.com>
2022-09-16 00:41:58 +0800

    将要进行git eject

--- src/App.tsx
+++ src/App.tsx
@@ -2,32 +2,63 @@ import React from "react";
 import { HashRouter as Router, Routes, Route, Link } from "react-router-dom";
 import styled from "styled-components";
 import "./index.scss";
+
+const AppView = styled.div`
+  height: 100vh;
+  width: 100vw;
+  display: flex;
+  flex-direction: column-reverse;
+`;
 const Nav = styled.div`
-  color: red;
+  height: 83px;
+  display: flex;
+  border: 1px solid red;
+  > ul {
+    flex: 1;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    > li {
+      flex: 1;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+    }
+  }
+`;
+const Content = styled.main`
+  flex: 1;
+  border: 1px solid yellow;
 `;
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <ul>
-          <li>
-            <Link to="/">Home</Link>
-          </li>
-          <li>
-            <Link to="/About">About</Link>
-          </li>
-          <li>
-            <Link to="/Users">Users</Link>
-          </li>
-        </ul>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/About" element={<About />} />
-          <Route path="/Users" element={<Users />} />
-          <Route path="/*" element={<NoMatch />} />
-        </Routes>
-      </Router>
+      <AppView>
+        <Router>
+          <Nav>
+            <ul>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/About">About</Link>
+              </li>
+              <li>
+                <Link to="/Users">Users</Link>
+              </li>
+            </ul>
+          </Nav>
+          <Content>
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/About" element={<About />} />
+              <Route path="/Users" element={<Users />} />
+              <Route path="/*" element={<NoMatch />} />
+            </Routes>
+          </Content>
+        </Router>
+      </AppView>
     </div>
   );
 }
